eac4c3df74903b281f7a0563f2d020ea1c8d3d48
David Forck <dgforck1@cougars.ccis.edu>
2015-02-18 18:47:23 -0600

    updated admin site

--- Dreadnaught/TTT/admin.py
+++ Dreadnaught/TTT/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from TTT.models import pending_games, game_results
+from TTT.models import pending_games, game_results, users, scripts, game
 
 # Register your models here.
+admin.site.register(users)
+admin.site.register(scripts)
+admin.site.register(game)
 admin.site.register(pending_games)
 admin.site.register(game_results)
