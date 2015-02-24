d9860279870ecfea2956f0bbc81520c0c81cf910
David Forck <dgforck1@cougars.ccis.edu>
2015-02-23 18:39:21 -0600

    we almost have it working!!!

--- Dreadnaught/TTT/forms.py
+++ Dreadnaught/TTT/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from TTT.models import scripts
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(label="Script Name", max_length = 50)
     file = forms.FileField()
     
+class SelectAI(forms.Form):
+    ai1 = forms.ModelChoiceField(label = "AI 1", queryset=scripts.objects.all())
+    ai2 = forms.ModelChoiceField(label = "AI 2", queryset=scripts.objects.all())
--- Dreadnaught/TTT/urls.py
+++ Dreadnaught/TTT/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import patterns, url
 from TTT import views
 
 urlpatterns = patterns('',
-    url(r'^play_game$', views.play_game, name='play_game'),
+    url(r'^play_game$', views.select_ai, name='play_game'),
+    url(r'^selectai$', views.select_ai, name='select_ai'),
+    url(r'^play_game$', views.select_ai, name='play_game'),
     url(r'^$', views.home, name='home'),
     url(r'^home/$', views.home, name='home'),
     url(r'^uploads$', views.uploads, name='uploads'),
