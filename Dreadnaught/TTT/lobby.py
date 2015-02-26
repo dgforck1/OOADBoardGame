5c5dc324c49c5139ad61824a69abed3a10cdbcbf
Travis Banks <tabanks3@cougars.ccis.edu>
2015-02-25 18:13:21 -0600

    Lobby

--- Dreadnaught/TTT/lobby.py
+++ Dreadnaught/TTT/lobby.py
@@ -3,44 +3,143 @@
 from TTT.models import users, scripts, game
 
 def create_html():
+    count = users.objects.count()
+    ai_count = scripts.objects.count()
+    game_count = game.objects.count()
+
     user = users.objects.get(pk=1)
     ai = scripts.objects.get(pk=1) 
     p = game(player1=user, state=0, ai1script=ai)
     p.save()
-    #ai_available = []
-    #pend_games = game.objects.filter(state=0)
-    #ai_on_server = scripts.objects.all()
-    #ai_available.append(ai_on_server)
+    
+    html_str = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    	<style type="text/css">
+    	.rwd-table {
+		  margin: 1em 0;
+		  min-width: 300px;
+		}
+		.rwd-table tr {
+		  border-top: 1px solid #ddd;
+		  border-bottom: 1px solid #ddd;
+		}
+		.rwd-table th {
+		  display: none;
+		}
+		.rwd-table td {
+		  display: block;
+		}
+		.rwd-table td:first-child {
+		  padding-top: .5em;
+		}
+		.rwd-table td:last-child {
+		  padding-bottom: .5em;
+		}
+		.rwd-table td:before {
+		  content: attr(data-th) ": ";
+		  font-weight: bold;
+		  width: 6.5em;
+		  display: inline-block;
+		}
+		@media (min-width: 480px) {
+		  .rwd-table td:before {
+		    display: none;
+		  }
+		}
+		.rwd-table th, .rwd-table td {
+		  text-align: left;
+		}
+		@media (min-width: 480px) {
+		  .rwd-table th, .rwd-table td {
+		    display: table-cell;
+		    padding: .25em .5em;
+		  }
+		  .rwd-table th:first-child, .rwd-table td:first-child {
+		    padding-left: 0;
+		  }
+		  .rwd-table th:last-child, .rwd-table td:last-child {
+		    padding-right: 0;
+		  }
+		}
 
-    
-    html_str = '<!DOCTYPE html> \
-    <html> \
-    <head> \
-    <meta chaarset=UTF-8> \
-    <title>Game Lobby</title> \
-    </head> \
-    <body bgcolor="#E6E6FA"> \
-    <h1>Game Lobby</h1> \
-    <table class="rwd-table"> \
-    <tr> \
-        <th>Movie Title</th> \
-        <th>Genre</th> \
-        <th>Year</th> \
-        <th>Gross</th> \
-    </tr> \
-    </table> \
-    <p>username ai name game id</p>'
-    
+		body {
+		  padding: 0 2em;
+		  font-family: Montserrat, sans-serif;
+		  -webkit-font-smoothing: antialiased;
+		  text-rendering: optimizeLegibility;
+		  color: #444;
+		  background: #eee;
+		}
+
+		h1 {
+		  font-weight: normal;
+		  letter-spacing: -1px;
+		  color: #34495E;
+		}
+
+		.rwd-table {
+		  background: #34495E;
+		  color: #fff;
+		  border-radius: .4em;
+		  overflow: hidden;
+		}
+		.rwd-table tr {
+		  border-color: #46627f;
+		}
+		.rwd-table th, .rwd-table td {
+		  margin: .5em 1em;
+		}
+		@media (min-width: 480px) {
+		  .rwd-table th, .rwd-table td {
+		    padding: 1em !important;
+		  }
+		}
+		.rwd-table th, .rwd-table td:before {
+		  color: #dd5;
+		}
+    	</style>
+    <meta chaarset=UTF-8>
+    <title>Game Lobby</title>
+    </head>
+    <body bgcolor="#E6E6FA">
+    <h1>Game Lobby</h1>
+    <table class="rwd-table">
+    <tr>
+        <th>Username</th>
+        <th>Ai Name</th>
+        <th>Game ID</th>
+    </tr>
+    """ 
     for games in game.objects.filter(state=0):
-        html_str += '<p>%s %s</p>' % (games.player1.user_name, p.ai1script.name)
-
-    html_str += '<h1>Available AI</h1>' 
+        html_str += """ 
+    	<tr>
+			<td data-th="Username">%s</td>
+			<td data-th="Ai Name">%s</td>
+			<td data-th="Game ID">%s</td>
+		</tr>
+        """ % (games.player1.user_name, p.ai1script.name, count)
+  
+    html_str += """
+    <h1>Available AI</h1>
+    <tr>
+    	<th>Available AI</th>
+    <tr>
+    """
 
     for AI in scripts.objects.all():
-        html_str += '<p>%s</p>' % (AI.name)
+        html_str += """
+        <tr>
+			<td data-th="Available AI">%s</td>
+		</tr>
+        """ % (AI.name)
     
-    html_str += '</body> \
-                 </html>' 
+    html_str += """
+	</table>
+    </body> 
+    </html>
+    """
 
     return html_str
 
